0295cb9503398bb4b9b1237f29d40b0ba336054e
sylviot <sylvio.tavares@hotmail.com>
2021-01-08 23:26:05 -0400

    Basic structure to route page

--- src/App.fs
+++ src/App.fs
@@ -1,14 +1,66 @@
 module App
 
-open Browser.Dom
+open Elmish
+open Elmish.React
+open Feliz
+open Feliz.Router
 
-// Mutable variable to count the number of times we clicked the button
-let mutable count = 0
 
-// Get a reference to our button and cast the Element to an HTMLButtonElement
-let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
+// APP
 
-// Register our listener
-myButton.onclick <- fun _ ->
-    count <- count + 1
-    myButton.innerText <- sprintf "Você clicou: %i vez(es)" count
+type Page =
+    | Home
+    | Overview
+    | Building
+    | Research
+    | Shipyard
+    | Defense
+    | Galaxy
+
+type State = { CurrentPage: Page }
+
+type Command =
+    | SetUsernameInput of string
+    | SwitchPage of Page
+
+let init() = { CurrentPage = Home }
+
+
+let update (command: Command) (state: State) = 
+    match command with
+    | SwitchPage nextPage -> { state with CurrentPage = nextPage }
+
+
+let render (state: State) (dispatch: Command -> unit) =
+    match state.CurrentPage with
+    | Home -> 
+        Html.div [
+               Html.h1 "OGame"
+               Html.h2 "Clone with F#"
+               Html.input [
+                   prop.placeholder "Username"
+                   //prop.onChange (fun _ -> SetUsernameInput >> dispacth)
+               ]
+               Html.button [
+                   prop.onClick (fun _ -> dispatch (SwitchPage Page.Overview))
+                   prop.text "Start game"
+               ]
+           ]
+    | Overview ->
+        Html.div [
+            Html.h1 "Overview"
+            Html.button [
+                prop.text "Building"
+                prop.onClick (fun _ -> dispatch (SwitchPage Page.Building))
+            ]
+        ]
+    | Building -> Html.h1 "Building"
+    | Research -> Html.h1 "Research"
+    | Shipyard -> Html.h1 "Shipyard"
+    | Defense -> Html.h1 "Defense"
+    | Galaxy -> Html.h1 "Galaxy"
+
+
+Program.mkSimple init update render
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
